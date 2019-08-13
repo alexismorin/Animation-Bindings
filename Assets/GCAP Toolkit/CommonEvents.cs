d81970693b93f61b270a606e6a3038e4f90f9f74
Alexis <alexis.morin.mail@gmail.com>
2019-08-13 16:27:29 -0400

    bindings

---
+++ Assets/GCAP Toolkit/CommonEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonEvents : MonoBehaviour
+{
+
+    void ParticleBurst()
+    {
+        GetComponent<ParticleSystem>().Emit(100);
+    }
+
+}
